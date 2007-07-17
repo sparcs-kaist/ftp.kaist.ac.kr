51a9777cfb533a80f04b80a9428f1f7c36e37920
netj <netj@sparcs.org>
2007-07-17 06:28:10 +0000

    모호하던 cvsup 서비스/동기화 구조 명확하게 개선함.  * get.cvsup 드라이버 만들어서 이제 source에 cvsup://을 쓰고 sync에서 get  $source를 하고 표준입력으로 SUP 파일 내용을 넣어주면 잘 작동함.  * amd64에서 x86 cvsup 돌리기 위한 부분을 최소화 했음.  cvsup이 잘 돌아가는  환경에서는 bin/cvsup만 없애주면 됨.
    git-svn-id: svn+ssh://project.sparcs.org/project/geoul/svn@196 8166199e-f512-0410-aa90-bfaab4d738d9

--- etc/cvsup/cvsup-mirror-init.sh
+++ etc/cvsup/cvsup-mirror-init.sh
@@ -10,20 +10,19 @@ read -p "Proceed?"
 [ "$REPLY" = y ] || exit 255
 
 set -e
+Base=`dirname "$0"`
 
 ## common
-mkdir $home_cvsupd                       # cvsupd home dir
-chown root:root $home_cvsupd
-chmod 755 $home_cvsupd
+mkdir ~cvsupd                       # cvsupd home dir
+chown root:root ~cvsupd
+chmod 755 ~cvsupd
 
-cd $home_cvsupd
-mkdir prefixes                        # cvsupd collection (data) dir
-mkdir sup                            # sup file dir
-chown root:root prefixes sup
-chmod 755 prefixes sup
-
+cd ~cvsupd
 
 ## FreeBSD
+mkdir FreeBSD
+pushd FreeBSD
+mkdir prefixes
 mkdir prefixes/FreeBSD.cvs                # CVS repository
 mkdir prefixes/FreeBSD-gnats.current      # GNATS DB
 mkdir prefixes/FreeBSD-mail.current       # mailing list archive
@@ -33,47 +32,68 @@ mkdir prefixes/distrib.self               # CVSup config file
 #mkdir prefixes/FreeBSD-jp-distrib.self    # CVSup config file (for JP ONLY)
 chown mirror:mirror prefixes/*
 chmod 755 prefixes/*
-mkdir sup/cvs-all
-mkdir sup/gnats
-mkdir sup/www
-mkdir sup/mail-archive
-mkdir sup/distrib
-#mkdir sup/jp-all                         # for JP ONLY
-#mkdir sup/jp-distrib                     # for JP ONLY
-chown mirror:mirror sup/*
-chmod 755 sup/*
-ln -s prefixes/distrib.self/sup sup-freebsd
+mkdir scan
+mkdir scan/cvs-all
+mkdir scan/gnats
+mkdir scan/www
+mkdir scan/mail-archive
+mkdir scan/distrib
+#mkdir scan/jp-all                         # for JP ONLY
+#mkdir scan/jp-distrib                     # for JP ONLY
+chown mirror:mirror scan/*
+chmod 755 scan/*
+popd
+ln -s FreeBSD/prefixes/distrib.self/sup sup-freebsd
 #ln -s prefixes/jp-distrib.self/sup-jp sup-jp  # for JP ONLY
 
 
 ## NetBSD
+mkdir NetBSD
+pushd NetBSD
+mkdir prefixes
 mkdir prefixes/NetBSD.cvs
 #mkdir prefixes/NetBSD-jp.cvs              # for JP ONLY
 mkdir prefixes/NetBSD-distrib.self
 chown mirror:mirror prefixes/*
 chmod 755 prefixes/*
-mkdir sup/netbsd
-mkdir sup/netbsd-distrib
-#mkdir sup/netbsd-jp-all                  # for JP ONLY
-#mkdir sup/netbsd-jp-distrib              # for JP ONLY
-chown mirror:mirror sup/*
-chmod 755 sup/*
-ln -s prefixes/NetBSD-distrib.self/sup-netbsd sup-netbsd
+mkdir scan
+mkdir scan/netbsd
+mkdir scan/netbsd-distrib
+#mkdir scan/netbsd-jp-all                  # for JP ONLY
+#mkdir scan/netbsd-jp-distrib              # for JP ONLY
+chown mirror:mirror scan/*
+chmod 755 scan/*
+popd
+ln -s NetBSD/prefixes/NetBSD-distrib.self/sup-netbsd sup-netbsd
+
 
 ## OpenBSD
+mkdir OpenBSD
+pushd OpenBSD
 mkdir prefixes/OpenBSD.cvs
 mkdir prefixes/OpenBSD-distrib.self
 chown mirror:mirror prefixes/*
 chmod 755 prefixes/*
-mkdir sup/OpenBSD-all
-mkdir sup/OpenBSD-distrib
-chown mirror:mirror sup/*
-chmod 755 sup/*
-ln -s prefixes/OpenBSD-distrib.self/sup-openbsd sup-openbsd
+mkdir scan/OpenBSD-all
+mkdir scan/OpenBSD-distrib
+chown mirror:mirror scan/*
+chmod 755 scan/*
+popd
+ln -s OpenBSD/prefixes/OpenBSD-distrib.self/sup-openbsd sup-openbsd
+
+
+## central
+mkdir prefixes                        # cvsupd collection (data) dir
+mkdir scan                            # scan file dir
+chown root:root prefixes scan
+chmod 755 prefixes scan
+cp $Base/update-prefixes+scan .
+chmod 755 update-prefixes+scan
+./update-prefixes+scan
 
 
 ## config files
-cat >sup/distrib/refuse.self <<EOF
+cat >scan/distrib/refuse.self <<EOF
 *.sh
 cvsupd.access
 cvsupd.passwd
@@ -81,8 +101,8 @@ prefixes
 sup.client
 supfile*
 EOF
-chmod 444 sup/distrib/refuse.self
-ln sup/distrib/refuse.self sup/netbsd-distrib/refuse.self 
-ln sup/distrib/refuse.self sup/OpenBSD-distrib/refuse.self 
+chmod 444 scan/distrib/refuse.self
+ln scan/distrib/refuse.self scan/netbsd-distrib/refuse.self 
+ln scan/distrib/refuse.self scan/OpenBSD-distrib/refuse.self 
 
 ln -sf /mirror/etc/cvsupd.access cvsupd.access 
