05b6e980bdd6a47676db1863d026258d63c0e290
netj <netj@sparcs.org>
2009-09-10 16:33:03 +0000

    GeoulMon: 기본 GWT/GAE 예제 코드로 틀 잡음.
    git-svn-id: svn+ssh://project.sparcs.org/project/geoul/svn@348 8166199e-f512-0410-aa90-bfaab4d738d9

---
+++ GeoulMon/src/org/sparcs/geoul/monitor/client/GreetingService.java
@@ -0,0 +1,12 @@
+package org.sparcs.geoul.monitor.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String name);
+}
