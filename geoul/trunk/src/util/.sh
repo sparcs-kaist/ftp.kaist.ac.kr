bd80f2e6f175a2f6ff3bdd3a0791fed8ff519201
netj <netj@sparcs.org>
2009-10-13 06:39:13 +0000

    GeoulCore: 디버깅을 돕기 위해 명령 스택 표시 기능 만듬.
     * set -u는 나중에 적용키로.
    
    
    git-svn-id: svn+ssh://project.sparcs.org/project/geoul/svn@404 8166199e-f512-0410-aa90-bfaab4d738d9

--- geoul/trunk/src/util/.sh
+++ geoul/trunk/src/util/.sh
@@ -6,9 +6,24 @@
 # Written by Jaeho Shin <netj@sparcs.org>.
 # (C) 2006-2009, Geoul Project. (http://project.sparcs.org/geoul)
 
-set -u # we shouldn't use undefined variables
+#set -u # we shouldn't use undefined variables
 
-[ -z "$GEOULDEBUG" ] || set -ex
+if [ -n "$GEOULDEBUG" ]; then
+    # show stack
+    n=`basename "$0"`
+    case "$n" in
+        main)
+        n=$(basename "$(dirname "$(readlink -f "$0")")")
+        n=${n%.d}
+        ;;
+    esac
+    case "$PS4" in
+        "+ ") PS4="+	$n> " ;;
+        *) PS4="${PS4% }$n> " ;;
+    esac
+    export PS4
+    set -x
+fi
 
 for-each-packages() {
     . "${GEOUL}for-each-packages" "$@"
